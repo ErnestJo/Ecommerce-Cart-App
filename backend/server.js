9bce1bcad8acbff24ea406fa2864b9c1007bcf6d
Ernest Joseph <smartlabstech@yahoo.com>
2021-07-29 09:53:16 +0300

    sedeer and server js

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,60 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
-const errorHandler = require('./middleware/error');
-const connectDB = require('./config/db');
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import morgan from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
+dotenv.config()
 
-//load env vars
-dotenv.config({ path: './config/config.env' });
+connectDB()
 
-// connect to Database
-connectDB();
+const app = express()
 
-
-const app = express();
-  
-// Body parser
-app.use(express.json());
-
-
-
-// Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
 
+app.use(express.json())
 
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
-app.use(errorHandler);
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
-const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-const server = app.listen(
-    PORT,
-    console.log(`Server running in ${process.env.PORT} mode on port ${PORT}`.yellow.bold)
-);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  // handle unhandle promise rejection
-  process.on('unhandledRejection', (err, Promise) => {
-    console.log(`Error: ${err.message}`);
-    // close down the server
-    server.close(() => process.exit(1));
-  
-  });
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
